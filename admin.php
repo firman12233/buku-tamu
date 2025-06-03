1af7bb23b5dacf23c41480bfb9ad8ea2b9258844
firman12233 <148781419+firman12233@users.noreply.github.com>
2025-06-03 07:59:18 +0700

    update

--- admin.php
+++ admin.php
@@ -43,6 +43,7 @@ $result = $conn->query($sql);
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Admin - Data Buku Tamu</title>
+    <link rel="icon" href="logo-smkn1slawi.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" />
